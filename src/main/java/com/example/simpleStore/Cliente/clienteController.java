ec2e8b16366fefb88421b861ee5b9ed2f4af7c37
Hanspeter <hansdietiker2012@gmail.com>
2023-10-17 14:33:07 -0300

    Produto Loja Simples

--- src/main/java/com/example/simpleStore/Cliente/clienteController.java
+++ src/main/java/com/example/simpleStore/Cliente/clienteController.java
@@ -2,8 +2,7 @@ package com.example.simpleStore.Cliente;
 
 import java.util.List;
 
-
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,7 @@ public class clienteController {
         this.icliente = icliente;
     }
 
-    @PostMapping("/")
+    @GetMapping("/")
     public List<clienteModel> getAllClientes() {
         return icliente.findAll();
 
