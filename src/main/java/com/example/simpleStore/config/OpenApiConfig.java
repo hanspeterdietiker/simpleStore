213a4315b6c52ad20849ea6426e421a6ee547aee
Hanspeter <hansdietiker2012@gmail.com>
2023-11-08 14:56:46 -0300

    att code - OpenApi

---
+++ src/main/java/com/example/simpleStore/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.example.simpleStore.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(
+        contact = @Contact(
+            name = "Hanspeter",
+            email =  "contactdevhanspeter@gmail.com"
+
+        ),
+        description = "OpenApi Documentação para Projeto ",
+        title= "OpenApi specificação - Hanspeter",
+        version = "1.0",
+        license = @License (
+            name = "Licence Hanspeter ",
+            url = "https://github.com/hanspeterdietiker/"
+        ),
+        termsOfService = "Termos de Serviço"
+    ),
+        servers = {
+            @Server(
+                description = "Server Local",
+                url = "http://localhost/8080"
+            )
+        }
+        
+)
+
+public class OpenApiConfig {
+    
+}
