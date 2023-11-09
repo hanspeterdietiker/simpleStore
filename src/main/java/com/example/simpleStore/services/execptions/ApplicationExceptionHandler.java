0779416f6190781f213e51149f3b903ccb2fd568
Hanspeter <hansdietiker2012@gmail.com>
2023-11-09 20:35:20 -0300

    att Execptions

--- src/main/java/com/example/simpleStore/services/execptions/ApplicationExceptionHandler.java
+++ src/main/java/com/example/simpleStore/services/execptions/ApplicationExceptionHandler.java
@@ -4,16 +4,16 @@ import jakarta.persistence.EntityNotFoundException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity nullExepction(Exception e){
-        return new ResponseEntity("Não encontrado no Banco de Dados", HttpStatus.BAD_GATEWAY);
+        return new ResponseEntity("Favor informar o dado em questão valido", HttpStatus.BAD_GATEWAY);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
